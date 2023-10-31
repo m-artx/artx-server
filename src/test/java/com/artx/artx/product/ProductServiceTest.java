4cef93d5c15fb31e48de10357720a330bf3f0a23
dlworhd <dlwodud821@gmail.com>
2023-10-31 23:49:17 +0900

    ARTX-2 init: Init Application

---
+++ src/test/java/com/artx/artx/product/ProductServiceTest.java
@@ -0,0 +1,16 @@
+//package com.artx.artx.product;
+//
+//import com.artx.artx.product.service.ProductService;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class ProductServiceTest {
+//
+//	@InjectMocks
+//	ProductService productService;
+//
+//	@Mock
+//}
