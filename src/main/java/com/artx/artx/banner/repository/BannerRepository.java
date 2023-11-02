f7575f9b4a6189558330e2b0a782c831f724127c
dlworhd <dlwodud821@gmail.com>
2023-11-02 17:20:23 +0900

    ARTX-16 feat: Implement Banner API

---
+++ src/main/java/com/artx/artx/banner/repository/BannerRepository.java
@@ -0,0 +1,17 @@
+package com.artx.artx.banner.repository;
+
+
+import com.artx.artx.banner.entity.Banner;
+import com.artx.artx.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface BannerRepository extends JpaRepository<Banner, Integer> {
+
+	@Query("SELECT b FROM Banner b LEFT JOIN FETCH b.product p ORDER BY b.createdAt ASC")
+	List<Banner> findAllWithProduct();
+
+	boolean existsByProduct(Product product);
+}
