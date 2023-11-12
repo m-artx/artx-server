5696e21cc915216b1851ab910d2a55d84add35be
dlworhd <dlwodud821@gmail.com>
2023-11-12 20:05:24 +0900

    ARTX-0 chore: Modify Image Address

--- src/main/java/com/artx/artx/common/config/SecurityConfig.java
+++ src/main/java/com/artx/artx/common/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.artx.artx.common.jwt.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
