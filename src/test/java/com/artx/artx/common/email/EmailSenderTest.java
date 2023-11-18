4263614ce3de4eb18b7b941166011a76d4ea377a
dlworhd <dlwodud821@gmail.com>
2023-11-18 15:49:36 +0900

    ARTX-100 refactor: Modify All Codes

--- src/test/java/com/artx/artx/common/email/EmailSenderTest.java
+++ src/test/java/com/artx/artx/common/email/EmailSenderTest.java
@@ -1,17 +1,17 @@
-package com.artx.artx.common.email;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmailSenderTest {
-
-	@Autowired
-	EmailSender emailSender;
-
-	@Test
-	void emailTest(){
-		emailSender.send();
-	}
-}
+//package com.artx.artx.common.email;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmailSenderTest {
+//
+//	@Autowired
+//	EmailSender emailSender;
+//
+//	@Test
+//	void emailTest(){
+//		emailSender.send();
+//	}
+//}
