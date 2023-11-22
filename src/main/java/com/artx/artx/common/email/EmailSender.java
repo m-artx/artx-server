d6fe639a63bcc95a7ed40c821b05af88c3a05642
dlworhd <dlwodud821@gmail.com>
2023-11-22 15:39:36 +0900

    ARTX-117 feat: Implement Image API & Add Krampoline Mount

--- src/main/java/com/artx/artx/common/email/EmailSender.java
+++ src/main/java/com/artx/artx/common/email/EmailSender.java
@@ -1,6 +1,7 @@
 package com.artx.artx.common.email;
 
-import jakarta.mail.MessagingException;
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.util.Properties;
 import java.util.UUID;
 
 @Component
@@ -26,22 +28,47 @@ public class EmailSender {
 	private String usersApiAddress;
 
 	public void sendAuthenticationEmail(String receiver, UUID userId) {
+		String host = "smtp.gmail.com";
+		int port = 587;
+
+		// 프록시 서버 호스트 및 포트
+		String proxyHost = "krmp-proxy.9rum.cc";
+		int proxyPort = 3128;
+
+		// 프록시 설정 추가
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", port);
+
+		// 프록시 설정 추가
+		props.put("mail.smtp.socks.host", proxyHost);
+		props.put("mail.smtp.socks.port", Integer.toString(proxyPort));
+
+		// 세션 생성
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
 		try {
+			// 메시지 생성 및 전송
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
+			message.setSubject("[ARTX] 회원가입 완료를 위해 인증이 필요합니다.");
+			message.setText("링크를 클릭하시면 인증이 완료됩니다.<a href=" + usersApiAddress + userId.toString() + "/email-auth\" + \">인증</a>");
 
-			MimeMessage mimeMessage = mailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+			Transport.send(message);
 
+			System.out.println("이메일 전송 완료.");
 
-			helper.setFrom(username);
-			helper.setTo(receiver);
-			helper.setSubject("[ARTX] 회원가입 완료를 위해 인증이 필요합니다.");
-			helper.setText("링크를 클릭하시면 인증이 완료됩니다.<a href=" + usersApiAddress + userId.toString() + "/email-auth" + ">인증</a>", true);
-
-			mailSender.send(mimeMessage);
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public void sendPasswordInitEmail(String receiver, String password) {
--- src/test/java/com/artx/artx/common/email/EmailSenderTest.java
+++ src/test/java/com/artx/artx/common/email/EmailSenderTest.java
@@ -1,17 +1,20 @@
 //package com.artx.artx.common.email;
 //
+//import jakarta.mail.*;
+//import jakarta.mail.internet.InternetAddress;
+//import jakarta.mail.internet.MimeMessage;
 //import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.Properties;
 //
 //@SpringBootTest
 //class EmailSenderTest {
 //
-//	@Autowired
-//	EmailSender emailSender;
 //
 //	@Test
-//	void emailTest(){
-//		emailSender.send();
+//	void emailTest() {
+//		// Gmail SMTP 서버 호스트 및 포트
+//
 //	}
 //}
